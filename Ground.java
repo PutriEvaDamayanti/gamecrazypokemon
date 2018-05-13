1b86ccc9709ce42b4ee3c9f4aea1b500307f7d28
Hendra97 <hendradarmawan5097@gmail.com>
2018-05-13 15:24:50 +0800

    perbaikan7

--- Ground.java
+++ Ground.java
@@ -18,5 +18,19 @@ public class Ground extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         setPaintOrder(Background.class,BolaBiru.class,Bola.class,Coin.class);
+        
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program. That is: create the initial
+     * objects and add them to the world.
+     * 
+     */
+    private void prepare()
+    {
+        Asphalt asphalt = new Asphalt();
+        addObject(asphalt, 290, 37);
+        Asphalt asphalt2 = new Asphalt();
+        addObject(asphalt2, 298, 114);
     }
 }
