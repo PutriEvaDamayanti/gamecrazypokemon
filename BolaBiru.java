b3d35cfdc53e001b7369eb6bcae58f048897df22
PutriEvaDamayanti <putrieva02@gmail.com>
2018-05-14 14:54:46 +0800

    perbaikan7

--- BolaBiru.java
+++ BolaBiru.java
@@ -12,8 +12,15 @@ public class BolaBiru extends Actor
      * Act - do whatever the BolaBiru wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int count=0;
+    World world;
     public void act() 
     {
         // Add your action code here.
+        setLocation(getX(),getY()+2);
+            world = getWorld();
+            if(isAtEdge()){
+                world.removeObject(this);
+            }
     }    
 }
