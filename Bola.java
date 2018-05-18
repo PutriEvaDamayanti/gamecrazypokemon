0b93bd53c5882b8e18b775d5ab9588ccd0847f48
PutriEvaDamayanti <putrieva02@gmail.com>
2018-05-18 13:20:15 +0800

    perbaikan13

--- Bola.java
+++ Bola.java
@@ -12,8 +12,13 @@ public class Bola extends Actor
      * Act - do whatever the Bola wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int count =0;
+    int score=0;
     public void act() 
     {
         // Add your action code here.
+        if(count==80){
+            getWorld().addObject(new BolaBiru(), Greenfoot.getRandomNumber(180)+200,0);
+            getWorld()
     }    
 }
